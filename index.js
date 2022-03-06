da02f6e334f06de1dff8ef65fbff335d8e0d4704
🢔⟬⧋⛛⧋⟭🢖 ⩹⌬⩺ <slavko.vuletic92@gmail.com>
2022-03-06 16:59:46 +0100

    trying to make it smaller

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
-const v_to_sha256 = require("./src");
+var crypto = require("crypto");
+const { notEmpty } = require("v_is_empty_value");
+
+const v_to_sha256 = async (value = null) => {
+  return (await notEmpty(value)) ? crypto.createHash("sha256").update(String(value)).digest("hex") : false;
+};
 
 module.exports = v_to_sha256;
